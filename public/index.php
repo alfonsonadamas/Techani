dd55ca9b33ecf8b0699cff5656ca3d230e53792d
Alfonso Gallardo <95257300+alfonsonadamas@users.noreply.github.com>
2023-09-09 19:24:46 -0600

    Estructura de pagina y archivo de conexion creado

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="build/styles.css" />
+    <title>Techani</title>
+  </head>
+  <body>
+    <img src="" alt="">
+    <h1>Bienvenido</h1>
+    <h2>Usuario</h2>
+    <h3>Apodo</h3>
+
+    <a href="registro.php">Registro Diario</a>
+    <a href="ejercicio.php">Registro de Ejercicio</a>
+    <a href="expediente.php">Añadir Expediente Clinico</a>
+    <a href="alimentos.php">Alimentos</a>
+    <a href="cuenta.php">Cuenta</a>
+  </body>
+</html>
