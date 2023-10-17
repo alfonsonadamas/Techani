38539e4df66f2db199d4da596b74262b291a9482
FJUM <ferjum200@gmail.com>
2023-10-16 21:11:35 -0600

    Alimentos añadidos

--- public/registro.php
+++ public/registro.php
@@ -8,15 +8,47 @@
     <title>Techani</title>
 </head>
 
-<body>
-    <h1>Registro diario</h1>
+<body class="flex font-['Noto Sans']">
+    <!-- barra_lateral -->
+    <div class="w-[300px] h-[1747px] bg-sky-600">
+        <div class="p-10 font-semibold">
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-amber-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Diario.png" alt="diario">
+                <a class="ml-2" href="registro.php">Registro Diario</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Alimentos.png" alt="diario">
+                <a class="ml-2" href="alimentos.php">Registro de Alimentos</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Ejercicio.png" alt="diario">
+                <a class="ml-2" href="ejercicio.php">Registro de Ejercicio</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Animo.png" alt="diario">
+                <a class="ml-2" href="">Estado de Animo</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Registros.png" alt="diario">
+                <a class="ml-2" class="ml-2" href="">Registros</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Alarmas.png" alt="diario">
+                <a class="ml-2" href="">Alarmas</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Citas.png" alt="diario">
+                <a class="ml-2" href="">Citas Médicas</a>
+            </div>
+        </div>
+    </div>
+    <h1 class=" w-[1447px] h-[87px] bg-sky-400 text-white text-2xl font-semibold text-center">Registro diario</h1>
     <form action="php/addRegistro.php" method="post" class="sm:flex sm:flex-row">
         <div>
             <div class="flex flex-col items-center mb-2 ">
                 <label for="tipo_insulina" class="block mb-1 text-sm font-medium text-gray-900 ">Tipo de
                     Insulina</label>
-                <select name="tipo_insulina" id=""
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-1.5 w-1/2">
+                <select name="tipo_insulina" id="" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-1.5 w-1/2">
                     <option value="lenta" class="">Lenta</option>
                     <option value="rapida">Rapida</option>
                 </select>
@@ -25,8 +57,7 @@
 
             <div class="flex flex-col mb-2">
                 <label for="dosis" class="block mb-1 text-sm font-medium text-gray-900">Dosis en ml</label>
-                <input type="number" name="dosis" min="1" max="25" placeholder="1" value="1"
-                    class="border-2 border-black p-1.5">
+                <input type="number" name="dosis" min="1" max="25" placeholder="1" value="1" class="border-2 border-black p-1.5">
             </div>
 
             <div class="flex flex-col">
@@ -54,8 +85,7 @@
         <div>
             <div class="flex flex-col">
                 <label for="vasos_agua" class="">Cantidad de agua consumida en vasos de 250ml</label>
-                <input type="number" name="vasos_agua" min="0" max="20" placeholder="0" value="0"
-                    class="border-2 border-black">
+                <input type="number" name="vasos_agua" min="0" max="20" placeholder="0" value="0" class="border-2 border-black">
             </div>
 
             <div class="flex flex-col">
@@ -68,23 +98,20 @@
                     <option value="otro" id="otro">Otro...</option>
                 </select>
 
-                <input type="text" id="especifique" placeholder="Especifique"
-                    class="hidden border-2 border-black resize-none ">
+                <input type="text" id="especifique" placeholder="Especifique" class="hidden border-2 border-black resize-none ">
 
                 <!-- <textarea name=""  cols="30" rows="10" ></textarea> -->
             </div>
 
             <div class="flex flex-col">
                 <label for="observaciones">Observaciones</label>
-                <textarea name="observaciones" id="" cols="30" rows="10"
-                    class="border-2 border-black resize-none"></textarea>
+                <textarea name="observaciones" id="" cols="30" rows="10" class="border-2 border-black resize-none"></textarea>
             </div>
         </div>
 
 
         <div>
-            <input type="submit"
-                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+            <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
         </div>
 
 
--- public/registroAlimentos.php
+++ public/registroAlimentos.php
@@ -4,56 +4,88 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="build/styles.css" />
     <title>Techani</title>
 </head>
 
-<body>
+<body class="flex font-['Noto Sans']">
+    <!-- barra_lateral -->
+    <div class="w-[300px] h-[1747px] bg-sky-600">
+        <div class="p-10 font-semibold">
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Diario.png" alt="diario">
+                <a class="ml-2" href="registro.php">Registro Diario</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-amber-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Alimentos.png" alt="diario">
+                <a class="ml-2" href="registroAlimentos.php">Registro de Alimentos</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Ejercicio.png" alt="diario">
+                <a class="ml-2" href="ejercicio.php">Registro de Ejercicio</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Animo.png" alt="diario">
+                <a class="ml-2" href="">Estado de Animo</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Registros.png" alt="diario">
+                <a class="ml-2" class="ml-2" href="">Registros</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Alarmas.png" alt="diario">
+                <a class="ml-2" href="">Alarmas</a>
+            </div>
+            <div class="pl-2 flex text-sm mb-5 w-[200px] h-[50px] bg-slate-200 rounded-[10px] items-center justify-start">
+                <img class="w-[30px] h-[30px]" src="img/barra_lateral/Citas.png" alt="diario">
+                <a class="ml-2" href="">Citas Médicas</a>
+            </div>
+        </div>
+    </div>
 
-    <h1>Registro Alimentos</h1>
-    <div class="flex flex-col">
-        <label for="Tipo">Tipo de alimento</label>
-        <select name="tipo_alimento" id="tipo_alimento" class="border-2 border-black" onchange="ocultar()">
-            <option value="Fruta">Fruta</option>
-            <option value="Leguminosa">Leguminosa</option>
-            <option value="Celeral">Celeral</option>
-            <option value="Lacteo">Lacteo</option>
-            <option value="Proteína">Proteína</option>
-            <option value="Grasa">Grasa</option>
-            <option value="Azucares">Azucares</option>
+    <div class="block">
+        <h1>Registro Alimentos</h1>
+        <div class="flex flex-col">
+            <label for="Tipo">Tipo de alimento</label>
+            <select name="tipo_alimento" id="tipo_alimento" class="border-2 border-black" onchange="ocultar()">
+                <option value="Fruta">Fruta</option>
+                <option value="Leguminosa">Leguminosa</option>
+                <option value="Celeral">Celeral</option>
+                <option value="Lacteo">Lacteo</option>
+                <option value="Proteína">Proteína</option>
+                <option value="Grasa">Grasa</option>
+                <option value="Azucares">Azucares</option>
+            </select>
+
+
+        </div>
+
+        <div class="flex flex-col">
+            <label for="Descripción alimento">Descripción del alimento</label>
+            <textarea name="Descripción alimento" id="" cols="30" rows="10" class="border-2 border-black resize-none"></textarea>
+        </div>
+
+        <label for="Tipo">Unidad de medida</label>
+        <select name="unidad_medida" id="unidad_medida" class="border-2 border-black" onchange="ocultar()">
+            <option value="Pieza">Pieza</option>
+            <option value="Gramos">Gramos</option>
+            <option value="Caja">Caja</option>
+            <option value="Vaso">Vaso</option>
         </select>
 
+        <div class="flex flex-col mb-2">
+            <label for="Cantidad porcion" class="block mb-1 text-sm font-medium text-gray-900">Cantidad por porción</label>
+            <input type="number" name="Cantidad porcion" min="1" max="100" placeholder="1" value="1" class="border-2 border-black p-1.5">
+        </div>
+        <div class="flex flex-col mb-2">
+            <label for="Carbohidratos porcion" class="block mb-1 text-sm font-medium text-gray-900">Carbohidratos por
+                porción</label>
+            <input type="number" name="Carbohidratos porcion" min="1" max="100" placeholder="1" value="1" class="border-2 border-black p-1.5">
+        </div>
 
-    </div>
-
-    <div class="flex flex-col">
-        <label for="Descripción alimento">Descripción del alimento</label>
-        <textarea name="Descripción alimento" id="" cols="30" rows="10"
-            class="border-2 border-black resize-none"></textarea>
-    </div>
-
-    <label for="Tipo">Unidad de medida</label>
-    <select name="unidad_medida" id="unidad_medida" class="border-2 border-black" onchange="ocultar()">
-        <option value="Pieza">Pieza</option>
-        <option value="Gramos">Gramos</option>
-        <option value="Caja">Caja</option>
-        <option value="Vaso">Vaso</option>
-    </select>
-
-    <div class="flex flex-col mb-2">
-        <label for="Cantidad porcion" class="block mb-1 text-sm font-medium text-gray-900">Cantidad por porción</label>
-        <input type="number" name="Cantidad porcion" min="1" max="100" placeholder="1" value="1"
-            class="border-2 border-black p-1.5">
-    </div>
-    <div class="flex flex-col mb-2">
-        <label for="Carbohidratos porcion" class="block mb-1 text-sm font-medium text-gray-900">Carbohidratos por
-            porción</label>
-        <input type="number" name="Carbohidratos porcion" min="1" max="100" placeholder="1" value="1"
-            class="border-2 border-black p-1.5">
-    </div>
-
-    <div>
-        <input type="submit"
-            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+        <div>
+            <input type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer">
+        </div>
     </div>
 
 </body>
