898b28b6015f08dbcbe4a155d3b091c0c31146c3
FJUM <ferjum200@gmail.com>
2023-11-15 12:45:51 -0600

    Subida de archivos

--- public/analisis.php
+++ public/analisis.php
@@ -16,39 +16,32 @@
             <div class="bg-azul w-1/4 mt-6 ml-6 mb-6 rounded-3xl flex flex-col items-center justify-center shadow-xl">
                 <img class="mb-10" src="img/Techani blanco con nombre.png" alt="" width="130">
                 <div class="">
-                    <div
-                        class="flex items-center h-10 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/registro.png" alt="" width="30">
                         <a class="text-white ml-3" href="">Registro Diario</a>
                     </div>
 
-                    <div
-                        class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/restaurante.png" alt="" width="30">
                         <a class="text-white ml-3" href="">Registro de Comidas</a>
                     </div>
-                    <div
-                        class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/comida-sana.png" alt="" width="30">
                         <a class="text-white ml-3" href="">Banco de Alimentos</a>
                     </div>
-                    <div
-                        class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/rutina-de-ejercicio.png" alt=" " width="30">
                         <a class="text-white ml-3" href="">Registro de Ejercicio</a>
                     </div>
-                    <div
-                        class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/carpeta.png" alt="" width="30">
                         <a class="text-white ml-3" href="">Análisis Clinicos</a>
                     </div>
-                    <div
-                        class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/alarma.png" alt="" width="30">
                         <a class="text-white ml-3" href="">Alarmas</a>
                     </div>
-                    <div
-                        class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
+                    <div class="flex items-center h-10 my-7 px-5 transition duration-500 ease-in-out hover:bg-azulSecundario rounded-lg">
                         <img src="img/calendario.png" alt="" width="30">
                         <a class="text-white ml-3" href="">Citas Médicas</a>
                     </div>
@@ -78,41 +71,32 @@
                     </div>
 
                     <div id="form" class="hidden mb-14">
-                        <form action=""
-                            class="border border-gray-400 w-2/4 rounded-lg my-7 ml-20 shadow-xl drop-shadow-lg p-6 flex flex-col items-center">
+                        <form enctype="multipart/form-data" action="php/addAnalisis.php" method="post" class="border border-gray-400 w-2/4 rounded-lg my-7 ml-20 shadow-xl drop-shadow-lg p-6 flex flex-col items-center">
                             <div class="flex mb-8">
                                 <div class="flex flex-col mr-20">
-                                    <label for="" class="font-semibold text-lg mb-2">Tipo de Estudio</label>
-                                    <input type="text" class="border border-black w-56 h-8">
+                                    <label for="tipo_estudio" class="font-semibold text-lg mb-2">Tipo de Estudio</label>
+                                    <input name="tipo_estudio" type="text" class="border border-black w-56 h-8">
                                 </div>
 
                                 <div class="flex flex-col ">
-                                    <label for="" class="font-semibold text-lg mb-2">Fecha</label>
-                                    <input type="date" class="border border-black w-40 h-8">
+                                    <label for="fecha" class="font-semibold text-lg mb-2">Fecha</label>
+                                    <input name="fecha" type="date" class="border border-black w-40 h-8">
                                 </div>
                             </div>
                             <div class="flex mb-5">
-                                <input
-                                    class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-                                    id="default_size" type="file">
+                                <input name="archivo_pdf" class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="default_size" type="file" accept=".pdf">
                             </div>
-                            <div
-                                class="w-full ml-2 mr-2 mb-5 p-4 flex flex-col border shadow-lg shadow-gray-500/50 border-slate-400 rounded-xl">
-                                <label for="observaciones"
-                                    class="block font-medium mb-1 text-gray-900 text-lg">Observaciones</label>
-                                <textarea name="observaciones" id="observaciones" cols="30" rows="10"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm p-1 rounded-lg block resize-none"
-                                    oninput="caracteresRestantes()" onkeydown="Enter(event)"></textarea>
+                            <div class="w-full ml-2 mr-2 mb-5 p-4 flex flex-col border shadow-lg shadow-gray-500/50 border-slate-400 rounded-xl">
+                                <label for="observaciones" class="block font-medium mb-1 text-gray-900 text-lg">Observaciones</label>
+                                <textarea name="observaciones" id="observaciones" cols="30" rows="10" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm p-1 rounded-lg block resize-none" oninput="caracteresRestantes()" onkeydown="Enter(event)"></textarea>
                                 <div id="contador-caracteres">Caracteres restantes: 300</div>
                             </div>
                             <div class="ml-4 mr-4">
-                                <input type="submit"
-                                    class="bg-amarillo text-black hover:bg-yellow-300font-bold py-2 px-4 cursor-pointer justify-end rounded-lg">
+                                <input type="submit" class="bg-amarillo text-black hover:bg-yellow-300font-bold py-2 px-4 cursor-pointer justify-end rounded-lg">
                             </div>
                         </form>
                     </div>
-                    <div
-                        class="border border-gray-400 w-11/12 rounded-lg shadow-xl drop-shadow-lg flex justify-center my-14">
+                    <div class="border border-gray-400 w-11/12 rounded-lg shadow-xl drop-shadow-lg flex justify-center my-14">
                         <div class="flex flex-col items-center my-7">
                             <img src="img/pdf.png" alt="" width="80">
                             <p>Titulo</p>
--- public/php/DBManager.php
+++ public/php/DBManager.php
@@ -1,28 +1,31 @@
 <?php
- class DBManager{
+class DBManager
+{
     private $key;
-    private function open(){
+    private function open()
+    {
         $link = mysqli_connect("127.0.0.1", "root", null, "techani") or die('Error connecting to Data Base');
         return $link;
     }
 
-    private function close($link){
+    private function close($link)
+    {
         mysqli_close($link);
     }
 
-    public function login($usuario, $contrasena){
+    public function login($usuario, $contrasena)
+    {
         $link = $this->open();
 
         $sql1 = "SELECT Cve_paciente FROM paciente WHERE correo=? AND contraseña=?";
-        
+
         $query = mysqli_prepare($link, $sql1) or die("Error at login");
-        $query -> bind_param("ss", $usuario, $contrasena);
-        $query -> execute();
+        $query->bind_param("ss", $usuario, $contrasena);
+        $query->execute();
         $result = $query->get_result();
 
         if ($result->num_rows == 1) {
             $response = array("success" => true, "redirect_url" => "principal.php");
-            
         } else {
             $response = array("success" => false, "message" => "Inicio de sesión fallido");
         }
@@ -30,88 +33,94 @@
         echo json_encode($response);
 
         $this->close($link);
-
     }
 
 
-    public function addRegistro( $tipo_insulina, $dosis, $tipo_dosis, $tipo_medicion , $agua, $dia_atipico ,$observaciones){
+    public function addRegistro($tipo_insulina, $dosis, $tipo_dosis, $tipo_medicion, $agua, $dia_atipico, $observaciones)
+    {
         $link = $this->open();
         $sql = "INSERT INTO registro_diario (Fecha_Hora, Tipo_insulina, Dosis ,Tipo_dosis, Tipo_medicion,Cantidad_agua, Dia_atipico, Observaciones, Cve_paciente ) VALUES (NOW(), ?, ?, ?,?, ?, ?, ?, 1);";
         $query = mysqli_prepare($link, $sql) or die("Error at login");
-        $query -> bind_param("sssssss", $tipo_insulina, $dosis,$tipo_dosis, $tipo_medicion, $agua ,$dia_atipico, $observaciones );
-        $query -> execute();
+        $query->bind_param("sssssss", $tipo_insulina, $dosis, $tipo_dosis, $tipo_medicion, $agua, $dia_atipico, $observaciones);
+        $query->execute();
         header("location: ../registro.php");
 
         $this->close($link);
-        
-        
     }
 
-    public function addRegistrAlimentoo($cadena) {
+    public function addAnalisis($fecha, $tipo_estudio, $observaciones, $archivo)
+    {
         $link = $this->open();
-       $query = mysqli_prepare($link, $cadena) or die("Error");
-       $query ->execute();
+        $sql = "INSERT INTO análisis_archivos (Fecha, `Tipo de estudio`, `Archivo pdf`, Imagen, Observaciones, Cve_paciente) VALUES (?, ?, ?, NULL, ?, 1);";
+        $query = mysqli_prepare($link, $sql) or die("Error at login");
+        $query->bind_param("ssss", $fecha, $tipo_estudio, $archivo, $observaciones);
+        $query->execute();
+        header("location: ../analisis.php");
 
         $this->close($link);
-
     }
 
-    public function verRegistrosDia(){
+    public function addRegistrAlimentoo($cadena)
+    {
         $link = $this->open();
-        
-        $sql = "SELECT idRegistro_diario, DATE_FORMAT(Fecha_Hora, '%d %b %Y %h:%i') AS Fecha_Formateada  FROM registro_diario WHERE DATE(Fecha_Hora) = CURDATE();";
+        $query = mysqli_prepare($link, $cadena) or die("Error");
+        $query->execute();
 
-        $result = $link ->query($sql);
-        
-        return $result;
-
+        $this->close($link);
     }
 
-    public function verRegistrosDiarios(){
+    public function verRegistrosDia()
+    {
         $link = $this->open();
-        
+
         $sql = "SELECT idRegistro_diario, DATE_FORMAT(Fecha_Hora, '%d %b %Y %h:%i') AS Fecha_Formateada  FROM registro_diario WHERE DATE(Fecha_Hora) = CURDATE();";
 
-        $result = $link ->query($sql);
+        $result = $link->query($sql);
 
         return $result;
     }
 
-    public function buscarAlimentosProteina(){
+    public function verRegistrosDiarios()
+    {
         $link = $this->open();
-        
+
+        $sql = "SELECT idRegistro_diario, DATE_FORMAT(Fecha_Hora, '%d %b %Y %h:%i') AS Fecha_Formateada  FROM registro_diario WHERE DATE(Fecha_Hora) = CURDATE();";
+
+        $result = $link->query($sql);
+
+        return $result;
+    }
+
+    public function buscarAlimentosProteina()
+    {
+        $link = $this->open();
+
         $sql = "SELECT * FROM `catalogo_alimentos` WHERE Tipo_alimento = 'Proteina';";
 
-        $result = $link ->query($sql);
+        $result = $link->query($sql);
 
         return $result;
     }
 
-    public function buscarAlimentosGrasa(){
+    public function buscarAlimentosGrasa()
+    {
         $link = $this->open();
-        
+
         $sql = "SELECT * FROM `catalogo_alimentos` WHERE Tipo_alimento = 'Grasa';";
 
-        $result = $link ->query($sql);
+        $result = $link->query($sql);
 
         return $result;
     }
 
-    public function buscarAlimentosLacteo(){
+    public function buscarAlimentosLacteo()
+    {
         $link = $this->open();
-        
+
         $sql = "SELECT * FROM `catalogo_alimentos` WHERE Tipo_alimento = 'Lacteo';";
 
-        $result = $link ->query($sql);
+        $result = $link->query($sql);
 
         return $result;
     }
-
-
-   
-
-
-    
- }
-
-?>
+}
